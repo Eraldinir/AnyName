fbad250788ee53c3d4e77c88fb1fc093ff91787b
valentin <eraldinir@gmail.com>
2023-03-05 19:56:50 +0300

    task 23 solved

--- Program.cs
+++ Program.cs
@@ -20,19 +20,30 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.Write("Enter X1 coordinate: ");
-int x1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter Y1 coordinate: ");
-int y1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter Z1 coordinate: ");
-int z1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter X1 coordinate: ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter Y1 coordinate: ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter Z1 coordinate: ");
+// int z1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Enter X2 coordinate: ");
-int x2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter Y2 coordinate: ");
-int y2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter Z2 coordinate: ");
-int z2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter X2 coordinate: ");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter Y2 coordinate: ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter Z2 coordinate: ");
+// int z2 = Convert.ToInt32(Console.ReadLine());
 
-double d = Math.Round(Math.Sqrt(Math.Pow(x1-x2, 2) + Math.Pow(y1-y2, 2) + Math.Pow(z1-z2, 2)), 2);
-Console.WriteLine("Distance = " + d);
+// double d = Math.Round(Math.Sqrt(Math.Pow(x1-x2, 2) + Math.Pow(y1-y2, 2) + Math.Pow(z1-z2, 2)), 2);
+// Console.WriteLine("Distance = " + d);
+//=================================================================================================================================
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine(Math.Pow(i,3));
+}
