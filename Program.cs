bb6545a4d45f528f1812bf4f66e0de5d736d4233
valentin <eraldinir@gmail.com>
2023-03-13 09:41:24 +0300

    task 29 done

--- Program.cs
+++ Program.cs
@@ -61,12 +61,23 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
-Console.WriteLine("Input number: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int count = 0;
-while (num > 0)
+// Console.WriteLine("Input number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int count = 0;
+// while (num > 0)
+// {
+//     count = count + num % 10;
+//     num = (num - (num % 10)) / 10;  
+// }
+// Console.WriteLine("Sum of elements = " + count);
+//=================================================================================================================================
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
 {
-    count = count + num % 10;
-    num = (num - (num % 10)) / 10;  
+    array[i] = new Random().Next(-1000, 1000); 
+    Console.Write(array[i] + ", "); 
 }
-Console.WriteLine("Sum of elements = " + count);
+//=================================================================================================================================
